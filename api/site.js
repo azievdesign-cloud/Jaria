69d360d0de3c71c5b3fb8321ee2f4c29c1e736aa
Tamerlan Aziev <azievdesign@gmail.com>
2026-08-14 12:11:36 +0300

    Refresh sequence deployment source

--- api/site.js
+++ api/site.js
@@ -1,4 +1,4 @@
-const SOURCE_URL = 'https://raw.githubusercontent.com/azievdesign-cloud/Jaria/main/index.html?rev=f3cd5db';
+const SOURCE_URL = 'https://raw.githubusercontent.com/azievdesign-cloud/Jaria/main/index.html?rev=fe23e05';
 
 export default async function handler(_request, response) {
   const source = await fetch(SOURCE_URL);
